12032f1de9c083a56f530fbf72f95750d6e49b2c
fdubois <&&yelamoko2019>
2019-12-09 14:32:05 +0100

    [FEAT]: Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule
